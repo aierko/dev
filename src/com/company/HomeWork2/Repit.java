ea56b186b2273ddbc4374b8df7f8d4dda90b191d
User <vtldtl80>
2017-07-13 17:31:05 +0300

    31

---
+++ src/com/company/HomeWork2/Repit.java
@@ -0,0 +1,31 @@
+package com.company.HomeWork2;
+
+import com.company.Lesson29_lists.List;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by User on 12.07.2017.Гласные и согласные буквы
+ * Написать программу, которая вводит с клавиатуры строку текста.
+ * Программа должна вывести на экран две строки:
+ * 1. первая строка содержит только гласные буквы
+ * 2. вторая - только согласные буквы и знаки препинания из введённой строки.
+ * Буквы соединять пробелом, каждая строка должна заканчиваться пробелом.
+ * public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+ * Пример ввода:
+ * Мама мыла раму.
+ * Пример вывода:
+ * а а ы а а у
+ * М м м л р м .
+ */
+public class Repit {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+
+    }
+
+    public static void method(List list) {
+
+    }
+}
