7bcb39107ced3d475db0d840b57d7f8693e668bc
User <vtldtl80>
2017-08-04 14:57:51 +0300

    36

---
+++ src/com/company/Operators/OperEq.java
@@ -0,0 +1,17 @@
+package com.company.Operators;
+
+/**
+ * Created by User on 04.08.2017.
+ */
+public class OperEq {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a == b = " + (a == b) );
+        System.out.println("a != b = " + (a != b) );
+        System.out.println("a > b = " + (a > b) );
+        System.out.println("a < b = " + (a < b) );
+        System.out.println("b >= a = " + (b >= a) );
+        System.out.println("b <= a = " + (b <= a) );
+    }
+}
