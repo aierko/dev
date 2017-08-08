f54dc173b77c2cfbc615f128d64dec21fbf012ed
User <vtldtl80>
2017-08-08 18:22:25 +0300

    37

--- src/com/company/Lesson107/Test05.java
+++ src/com/company/Lesson107/Test05.java
@@ -1,12 +1,23 @@
 package com.company.Lesson107;
 
+import java.io.*;
+
 /**
  * Created by User on 08.08.2017.
  * Задача: Пользователь вводит два имени файла. И копирует первый файл на место заданное вторым именем.
  */
 public class Test05 {
-    public static void main(String[] args) {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+    public static void main(String[] args) throws IOException {
+          String file1 = reader.readLine();
+          String file2 = reader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(file1);
+        FileOutputStream fileOutputStream = new FileOutputStream(file2);
+
+        while (fileInputStream.available() > 0){
+            fileOutputStream.write(fileInputStream.read());
+        }
     }
 
 }
