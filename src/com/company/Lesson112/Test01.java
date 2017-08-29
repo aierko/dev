1688290904a7d365d8b6998df8933f0c58f0b00f
User <vtldtl80>
2017-08-29 21:31:42 +0300

    41

--- src/com/company/Lesson112/Test01.java
+++ src/com/company/Lesson112/Test01.java
@@ -4,26 +4,34 @@ import java.util.Arrays;
 
 /**
  * Created by User on 27.08.2017.
- *  * You are given a array of n-1 integers and these integers are in the range of 0 to n.
+ * * You are given a array of n-1 integers and these integers are in the range of 0 to n.
  * There are no duplicates in array. One of the integers is missing in the array. Write an efficient code to find the missing integer.
  * Example:
- I/P    [0, 1, 2, 4, 6, 3, 7, 8]
- O/P    5
-
-  1 отсортировать массив для порядка элементов
-  2 создать переменную findeNumbers
-  3 пройтись фором по массиву
-  4  записать в переменную findeNumbers
-
+ * I/P    [0, 1, 2, 4, 6, 3, 7, 8]
+ * O/P    5
+ * Вам задан массив из n-1 целых чисел, и эти целые числа находятся в диапазоне от 0 до n.
+ *   * В массиве нет дубликатов. В массиве отсутствует одино из целых чисел. Напишите эффективный код, чтобы найти недостающее целое число.
+ * <p>
+ * 1 отсортировать массив для порядка элементов
+ * 2 создать переменную findeNumbers
+ * 3 пройтись фором по массиву
+ * 4  записать в переменную findeNumbers
  */
 public class Test01 {
     public static void main(String[] args) {
-
+        int[] array = {0, 1, 2, 4, 5, 6, 3, 7, 8, 9, 10}; // 0,1,2,3,4,5,6,7,8;
+        System.out.println(returnArrayNumber(array));
     }
-    public static int returnArrayNumber(int[] a){
+
+
+    public static int returnArrayNumber(int[] a) {
         Arrays.sort(a);
-        int x = 0;
-
-        return x;
+        for (int i = 0; i < a.length-1; i++) { //0 7    9
+            if (a[i] + 1 != a[i + 1]){
+                return a[i] + 1;
+            }
+        }
+        return a.length;
     }
+
 }
---
+++ src/com/company/Lesson113/Test01.java
@@ -0,0 +1,38 @@
+package com.company.Lesson113;
+
+import java.util.Arrays;
+
+/**
+ * Created by User on 28.08.2017.
+ *   You have an array a containing 2  n + 2 positive numbers,
+ *   in which n numbers occur twice and two other numbers occur only once and are distinct.
+ * Find the two distinct numbers and return them in ascending order.
+ * У вас есть массив a, содержащий 2 n + 2 положительных числа,
+   *, в котором n чисел встречаются дважды, а два других числа встречаются только один раз и различны.
+   * Найдите два разных числа и верните их в порядке возрастания.
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        int [] array = {2,23,45,23,45,6,76,76};
+         array = returnArray(array);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+    public static int[] returnArray(int []array){
+        Arrays.sort(array);
+        int [] array1 = new int[2];
+        for (int i : array) {
+            int count = 0;
+            for (int i1 : array) {
+                if (i == i1){
+                    count ++;
+                }
+            }
+            if (count == 1){
+                array[]
+            }
+        }
+        return array1;
+    }
+}
